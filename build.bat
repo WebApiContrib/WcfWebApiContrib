3865d701483495b35d9b6f91310169488e7e0e1a
Ryan Riley <ryan.riley@panesofglass.org>
2010-11-09 10:38:45 -0800

    Updated build.bat to set up the environment when not in a .net console.

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
+C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat
+cls
 msbuild .\wcf\Http\Http.sln
 msbuild .\wcf\WCFJQuery\WcfJQuery.sln
 msbuild .\Samples\SelfHosted\server\SelfHostedServer.sln
