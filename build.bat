90bfe659e3209a40744aa6526b9e8af1896f6073
Ryan Riley <ryan.riley@panesofglass.org>
2011-01-20 22:01:27 -0800

    Fixed build script.

--- build.bat
+++ build.bat
@@ -3,4 +3,4 @@ msbuild .\wcf\Http\Http.sln
 msbuild .\wcf\WCFJQuery\WcfJQuery.sln
 msbuild .\wcf\Http\Http.sln /p:Configuration=Release
 msbuild .\wcf\WCFJQuery\WcfJQuery.sln /p:Configuration=Release
-msbuild .\Samples\SelfHosted\server\SelfHostedServer.sln
+msbuild .\Samples\Samples.sln
