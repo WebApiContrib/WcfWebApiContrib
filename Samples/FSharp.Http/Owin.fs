9b8076fac494d341e4461eaffd03d15a51693446
Ryan Riley <ryan.riley@panesofglass.org>
2011-01-24 08:51:53 -0800

    Merged the FSharp.Http types into a single file for portability and removed the FSharp.Http project. Also cleaned up some minor syntax issues.

--- Samples/FSharp.Http/Owin.fs
+++
@@ -1,7 +0,0 @@
-﻿namespace Owin
-open System
-open System.Collections.Generic
-open System.Net.Http
-
-// NOTE: This is not the actual OWIN definition of an application, just a close approximation.
-type Application = Action<HttpRequestMessage, Action<string, seq<KeyValuePair<string,string>>, byte[]>, Action<exn>>
