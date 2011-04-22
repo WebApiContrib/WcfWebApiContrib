f239298f78d672855db31b69c9ab4a70e7b1b576
Ryan Riley <ryan.riley@panesofglass.org>
2011-04-22 13:49:17 -0700

    Updated wcf subrepo to the latest tip on codeplex.

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 cls
-msbuild .\wcf\Http\Http.sln
+msbuild .\"wcf\WCFWebAPI\WCF Web API - All.sln"
 msbuild .\wcf\WCFJQuery\WcfJQuery.sln
-msbuild .\wcf\Http\Http.sln /p:Configuration=Release
+msbuild .\"wcf\WCFWebAPI\WCF Web API - All.sln" /p:Configuration=Release
 msbuild .\wcf\WCFJQuery\WcfJQuery.sln /p:Configuration=Release
 msbuild .\Samples\Samples.sln
